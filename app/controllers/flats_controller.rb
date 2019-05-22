3fa45dc98d17e8b1ebb9d0ff621db71187cfec5e
Michael Lauritsch <ML62686@gmail.com>
2019-05-22 17:55:00 +0800

    Completed Simple Airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,34 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :number_of_guests, :price_per_night)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.destroy_all
+
+require 'faker'
+9.times do
+  Flat.create!(
+  name: "#{Faker::FunnyName.name}",
+  address: "10 #{Faker::Address.street_name} #{Faker::Address.city} #{Faker::Address.zip_code}",
+  description: "#{Faker::ChuckNorris.fact}",
+  price_per_night: rand(1..75),
+  number_of_guests: rand(1..5)
+)
+end
